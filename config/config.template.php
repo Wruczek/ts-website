8e00244ecf135f6e2da3ee7c9dab2fe9bee0d20b
Wruczek <wruczekk@gmail.com>
2016-07-11 17:04:37 +0200

    Added contact title

--- config/config.template.php
+++ config/config.template.php
@@ -53,10 +53,13 @@ $config["adminlist"] = array(6, 17, 19);
 
 /************* Konfiguracja panelu "kontakt" *************/
 
-// Ustaw $config['contact'] = ''; by kompletnie wyłączyć wyświetlanie panelu
-$config['contact'] = '
+/************* Konfiguracja panelu "kontakt" *************/
+
+// Ustaw $config['contact']['text'] = ''; by kompletnie wyłączyć wyświetlanie panelu
+$config['contact']['title'] = 'Kontakt z właścicielem';
+$config['contact']['text'] = '
 <li>TeamSpeak: <span class="pullright">kanał <a href="ts3server://ts.wruczek.top?channel=Kana%C5%82y%20publiczne%2FPogaduchy%2FPogaduchy%205">Pomoc</a></span></li>
 <li>Email: <span class="pullright"><a href="mailto:kontakt@email.com">konakt@email.com</a></span></li>
-<li>GaduGadu: <span class="pullright"><a href="gg:49568758">49568758 <img src="http://status.gadu-gadu.pl/users/status.asp?id=49568758&styl=1" alt="GG status"></a></span></li>
-<li>Steam: <span class="pullright"><a href="http://steamcommunity.com/id/wruczek">Wruczek</a></li>
+<li>GaduGadu: <span class="pullright"><a href="gg:49568758">49568758 <img src="http://status.gadu-gadu.pl/users/status.asp?id=49568758&styl=1"></a></span></li>
+<li>Steam: <span class="pullright"><a href="http://steamcommunity.com/id/wruczek">Wruczek</a></span></li>
 ';
