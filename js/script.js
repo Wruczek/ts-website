65b0a0a75ede452eba454af595468249bc836982
Wruczek <wruczekk@gmail.com>
2016-07-24 00:32:46 +0200

    v 1.2.4

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ $(function () {
 $('.news-body').readmore({
     speed: 500,
     collapsedHeight: 300,
-    moreLink: '<button type="button" class="btn btn-info">Pokaż wiecej</button>',
-    lessLink: '<button type="button" class="btn btn-info">Pokaż mniej</button>'
+    moreLink: '<button type="button" class="btn btn-dark">Pokaż wiecej</button>',
+    lessLink: '<button type="button" class="btn btn-dark">Pokaż mniej</button>'
 });
