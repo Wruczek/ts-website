f072b95a201192c2d351296cac8754e68e24c98f
Wruczek <wruczekk@gmail.com>
2016-10-20 21:02:31 +0200

    Added fa-fw to normalize space taken by icons

--- js/status.js
+++ js/status.js
@@ -28,19 +28,19 @@ function checkStatus() {
                 var averagePing = Math.round(json.averagePing * 100) / 100;
 
                 result =
-                    '<p><i class="fa fa-power-off" aria-hidden="true"></i> ' + statusOnline + ': <span class="label label-success">' + clientsonline + ' / ' + maxclients + ' (' + clientsprecent + '%)</span></p>' +
-                    '<p><i class="fa fa-clock-o" aria-hidden="true"></i> ' + statusUptime + ': <span class="label label-success">' + uptime + '</span></p>' +
-                    '<p><i class="fa fa-info-circle" aria-hidden="true"></i> ' + statusVersion + ': <span class="label label-success">' + version + ' @ ' + platform + '</span></p>' +
-                    '<p><i class="fa fa-signal" aria-hidden="true"></i> ' + statusAvgping + ': <span class="label label-success">' + averagePing + ' ms</span></p>' +
-                    '<p><i class="fa fa-bolt" aria-hidden="true"></i> ' + statusAvgpl + ': <span class="label label-success">' + averagePacketloss + '%</span></p>';
+                    '<p><i class="fa fa-power-off fa-fw" aria-hidden="true"></i> ' + statusOnline + ': <span class="label label-success">' + clientsonline + ' / ' + maxclients + ' (' + clientsprecent + '%)</span></p>' +
+                    '<p><i class="fa fa-clock-o fa-fw" aria-hidden="true"></i> ' + statusUptime + ': <span class="label label-success">' + uptime + '</span></p>' +
+                    '<p><i class="fa fa-info-circle fa-fw" aria-hidden="true"></i> ' + statusVersion + ': <span class="label label-success">' + version + ' @ ' + platform + '</span></p>' +
+                    '<p><i class="fa fa-signal fa-fw" aria-hidden="true"></i> ' + statusAvgping + ': <span class="label label-success">' + averagePing + ' ms</span></p>' +
+                    '<p><i class="fa fa-bolt fa-fw" aria-hidden="true"></i> ' + statusAvgpl + ': <span class="label label-success">' + averagePacketloss + '%</span></p>';
             } else {
-                result = '<p><i class="fa fa-power-off" aria-hidden="true"></i> Online: <span class="label label-danger">' + statusOffline + '</span></p>';
+                result = '<p><i class="fa fa-power-off fa-fw" aria-hidden="true"></i> Online: <span class="label label-danger">' + statusOffline + '</span></p>';
             }
 
             $("#serverstatus").html(result);
         },
         error: function (result) {
-            $("#serverstatus").html('<p><i class="fa fa-power-off" aria-hidden="true"></i> ' + statusOnline + ': <span class="label label-danger">ERROR</span></p>');
+            $("#serverstatus").html('<p><i class="fa fa-power-off fa-fw" aria-hidden="true"></i> ' + statusOnline + ': <span class="label label-danger">ERROR</span></p>');
         }
     })
 }
