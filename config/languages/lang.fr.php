f421ff50fec28c79663476754ad06fd6d38bcc7c
Kévin <kevinfleury.todae@gmail.com>
2017-01-21 15:39:03 +0100

    Converting HTML hexadecimal to HTML value

--- config/languages/lang.fr.php
+++ config/languages/lang.fr.php
@@ -9,7 +9,7 @@ $lang = array();
 /************* General *************/
 
 $lang["general"]["langcode"] = "fr_FR";
-$lang["general"]["scripterror"] = "Une erreur s&#039;est produite ! {0} : {1}";
+$lang["general"]["scripterror"] = "Une erreur s'est produite ! {0} : {1}";
 
 /************* Navbar *************/
 
@@ -17,16 +17,16 @@ $lang["navbar"]["navigation"] = "Navigation";
 
 $lang["navbar"]["viewer"] = "Affichage du Serveur";
 $lang["navbar"]["bans"] = "Liste des Bans";
-$lang["navbar"]["rules"] = "R&egrave;gles du Serveur";
+$lang["navbar"]["rules"] = "Règles du Serveur";
 
 $lang["navbar"]["connect"] = "Se connecter au Serveur";
-$lang["navbar"]["connecttooltip"] = "Cliquez pour vous connecter &agrave; {0}";
+$lang["navbar"]["connecttooltip"] = "Cliquez pour vous connecter à {0}";
 
 
 /************* Footer *************/
 
 $lang["footer"]["css"] = "CSS par";
-$lang["footer"]["background"] = "arri&egrave;re-plan";
+$lang["footer"]["background"] = "arrière-plan";
 
 
 /************* Server status *************/
@@ -45,12 +45,12 @@ $lang["serverstatus"]["avgpl"] = "Perte moyenne de paquets";
 
 /************* Admin list *************/
 
-$lang["adminlist"]["title"] = "Liste d&#039;Admin";
+$lang["adminlist"]["title"] = "Liste d'Admin";
 $lang["adminlist"]["emptygroup"] = "Ce groupe est vide";
 $lang["adminlist"]["status"]["online"] = "En ligne";
 $lang["adminlist"]["status"]["away"] = "Away";
 $lang["adminlist"]["status"]["offline"] = "Hors ligne";
-$lang["adminlist"]["lastupdate"] = "Derni&egrave;re mise &agrave; jour : {0}";
+$lang["adminlist"]["lastupdate"] = "Dernière mise à jour : {0}";
 
 
 /************* Server viewer promo box (under adminlist and contact) *************/
@@ -70,14 +70,14 @@ $lang["index"]["title"] = "News";
 $lang["index"]["showmore"] = "Lire la suite";
 $lang["index"]["showless"] = "Moins montrer";
 
-$lang["index"]["errortitle"] = "Erreur : le dossier des news n&#039;a pas &eacute;t&eacute; trouv&eacute; !";
-$lang["index"]["errorsubtitle"] = "Assurez-vous que l&#039;emplacement du répertoire défini dans <b><code>config/config.php</code></b> est valide.";
+$lang["index"]["errortitle"] = "Erreur : le dossier des news n'a pas été trouvé !";
+$lang["index"]["errorsubtitle"] = "Assurez-vous que l'emplacement du répertoire défini dans <b><code>config/config.php</code></b> est valide.";
 
 
 /************* Server viewer - viewer.php *************/
 
 $lang["viewer"]["title"] = "Affichage du Serveur";
-$lang["viewer"]["lastupdate"] = "Derni&egrave;re mise &agrave; jour : {0}";
+$lang["viewer"]["lastupdate"] = "Dernière mise à jour : {0}";
 
 
 /************* Ban List - bans.php *************/
@@ -85,7 +85,7 @@ $lang["viewer"]["lastupdate"] = "Derni&egrave;re mise &agrave; jour : {0}";
 $lang["banlist"]["title"] = "Liste des Bans";
 $lang["banlist"]["datatablesurl"] = "//cdn.datatables.net/plug-ins/1.10.12/i18n/French.json";
 $lang["banlist"]["emptylist"] = "LA LISTE DES BANS EST VIDE";
-$lang["banlist"]["lastupdate"] = "Derni&egrave;re mise &agrave; jour : {0}";
+$lang["banlist"]["lastupdate"] = "Dernière mise à jour : {0}";
 
 $lang["banlist"]["table"]["emptyreason"] = "(pas de raison)";
 $lang["banlist"]["table"]["permaban"] = "Jamais";
@@ -98,6 +98,6 @@ $lang["banlist"]["table"]["expires"] = "Expire";
 
 /************* Rules - rules.php *************/
 
-$lang["rules"]["title"] = "R&egrave;gles du Serveur";
-$lang["rules"]["filenotfound"] = "Erreur : Le fichier <code>config/rules.md</code> n&#039;a pas &eacute;t&eacute; trouv&eacute; !";
-$lang["rules"]["readerror"] = "Erreur : Impossible d&#039;acc&eacute;der au fichier <code>config/rules.md</code> !";
+$lang["rules"]["title"] = "Règles du Serveur";
+$lang["rules"]["filenotfound"] = "Erreur : Le fichier <code>config/rules.md</code> n'a pas été trouvé !";
+$lang["rules"]["readerror"] = "Erreur : Impossible d'accéder au fichier <code>config/rules.md</code> !";
