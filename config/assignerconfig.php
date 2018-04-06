168b04b5c378161c5727e68ffac363fc4fb15d99
Oliwier <minecraft.thomas234@gmail.com>
2018-04-07 01:14:03 +0200

    Create assignerconfig.php

---
+++ config/assignerconfig.php
@@ -0,0 +1,14 @@
+<?php
+
+$assignerconfig = [
+    [
+        "name" => "Age", // - Name of group
+        "limit" => 1, // - Group limit
+        "sgids" => [9,10,12,14] // - Grop ids
+    ],
+    [
+        "name" => "Games", // - Name of group
+        "limit" => 4, // - Group limit
+        "sgids" => [6,7,8] // - Grop ids
+    ]
+];
