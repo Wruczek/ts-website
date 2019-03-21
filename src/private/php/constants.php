acb163302c58de0be5be6b12594f9bb23c0fe517
Wruczek <wruczekk@gmail.com>
2019-03-21 03:43:06 +0100

    Version bump

--- src/private/php/constants.php
+++ src/private/php/constants.php
@@ -1,5 +1,5 @@
 <?php
-define("__TSWEBSITE_VERSION",   "dev-2.0.2");
+define("__TSWEBSITE_VERSION",   "dev-2.0.3");
 define("__TSWEBSITE_COMMIT",    "no-commit");
 define("__BASE_DIR",            __DIR__ . "/../..");
 define("__PRIVATE_DIR",         __BASE_DIR . "/private");
