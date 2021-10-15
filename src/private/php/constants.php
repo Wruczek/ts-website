81b01f657582b5acfc0e9ca4fa852ebc892b2b32
toster234 <contact@toster.dev>
2021-10-16 01:13:09 +0200

    Bump version

--- src/private/php/constants.php
+++ src/private/php/constants.php
@@ -1,5 +1,5 @@
 <?php
-define("__TSWEBSITE_VERSION",   "dev-2.0.5.2");
+define("__TSWEBSITE_VERSION",   "dev-2.0.6");
 define("__TSWEBSITE_COMMIT",    "no-commit");
 define("__BASE_DIR",            __DIR__ . "/../..");
 define("__PRIVATE_DIR",         __BASE_DIR . "/private");
